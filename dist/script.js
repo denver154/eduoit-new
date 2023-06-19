9b29b0c4dded1c2fbbd5aeaf6e1c128f13062c42
denver154 <realouise175@gmail.com>
2023-06-19 14:27:10 +0800

    second update

--- dist/script.js
+++ dist/script.js
@@ -46,3 +46,24 @@ function removeActiveContent() {
     nav.classList.remove("active");
   });
 }
+
+// When the user clicks on the button, scroll to the top of the document
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
